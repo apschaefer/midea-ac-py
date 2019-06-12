27d6672ed25af15be12aeea611ae999519534ac6
clorox <clorox@clorox.duckdns.org>
2019-06-12 11:36:07 -0400

    Attempted to merge throttling from https://github.com/Michael0yodi/midea-ac-py/midea.py

--- climate.py
+++ climate.py
@@ -17,6 +17,9 @@ from homeassistant.components.climate.const import (
     SUPPORT_AWAY_MODE, SUPPORT_FAN_MODE, SUPPORT_OPERATION_MODE, SUPPORT_SWING_MODE,
     SUPPORT_ON_OFF)
 from homeassistant.const import CONF_USERNAME, CONF_PASSWORD, TEMP_CELSIUS, TEMP_FAHRENHEIT, ATTR_TEMPERATURE
+from homeassistant.util import Throttle
+from datetime import timedelta
+
 
 REQUIREMENTS = ['midea==0.1.7', 'pycryptodome==3.7.0']
 VERSION = '0.1.7'
@@ -26,6 +29,7 @@ _LOGGER = logging.getLogger(__name__)
 CONF_APP_KEY = 'app_key'
 CONF_TEMP_STEP = 'temp_step'
 CONF_INCLUDE_OFF_AS_STATE = 'include_off_as_state'
+SCAN_INTERVAL = timedelta(seconds=120)
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_APP_KEY): cv.string,
@@ -90,6 +94,16 @@ class MideaClimateACDevice(ClimateDevice):
 
     async def async_update(self):
         """Retrieve latest state from the appliance if no changes made, 
+        otherwise update the remote device state."""
+        if self._changed:
+            await self.hass.async_add_executor_job(self._device.apply)
+            self._changed = False
+        else:
+            await self.hass.async_add_executor_job(self._device.refresh)
+    #test throttle..
+    @Throttle(SCAN_INTERVAL)
+    async def async_update(self):
+        """Retrieve latest state from the appliance if no changes made,
         otherwise update the remote device state."""
         if self._changed:
             await self.hass.async_add_executor_job(self._device.apply)
